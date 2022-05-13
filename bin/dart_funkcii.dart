57be107cb3b9eb8f5f5f1f27dca0b7a1211b6f55
Samudinov <104208404+Samudinov@users.noreply.github.com>
2022-05-13 20:07:10 +0600

    first commit

---
+++ bin/dart_funkcii.dart
@@ -0,0 +1,47 @@
+void main(List<String> arguments) {
+  print(functionIn2(a:2)); 
+  
+  print(functionPluse(a:2, c:3));
+  
+  print(functionTri(a:2, b:3, c:5));
+ 
+  print(funcTime(min:2));
+  
+  List array = [1, 3, 4, 5, 6];
+  print(funcFour(array: array));
+  
+  bool a = false;
+ print (func(a: a));
+ 
+ int b = 1; 
+ 
+ print(funcb (b: b));
+}
+
+int functionIn2({required int a}) => a * a;
+
+
+int functionPluse({required int a, required int c}) =>a + c;
+
+
+double functionTri ({required int a, required int b, required double c}) => (a + b) / c;
+
+int funcTime ({required int min}) => min * 60 ;
+
+int funcFour ({required List array}) => array[0];
+
+String func ({required bool a}) {
+  if (a) {
+  return ('значение тру');
+  } else  {
+  return('false');}
+}
+
+bool funcb ({required int b}) { 
+  if (b<= 0) {
+    return false;
+  } else 
+   return true;
+
+
+}
---
+++ test/dart_funkcii_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_funkcii/dart_funkcii.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
